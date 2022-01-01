d6afbe3085d81de629315beaa5e9fea62109baf9
Grubba27 <70247653+Grubba27@users.noreply.github.com>
2022-01-01 20:17:05 -0300

    continued type studies

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,11 @@ use rand::Rng;
 fn main() {
     println!("Chute um número");
     let secret_number: u32 = rand::thread_rng().gen_range(1..101);
+    let x = String::from("Teste");
+    println!("{}", x);
+    println!("{}", &x);
+    // print_type_of(x);
+    print_type_of(&x);
 
     loop {
         println!("Começe colocando o número que quer");
@@ -28,6 +33,8 @@ fn main() {
             Ordering::Greater => println!("Está maior"),
         }
     }
+
+
 }
 fn print_type_of<T>(_: &T) {
     println!("{}", std::any::type_name::<T>())
