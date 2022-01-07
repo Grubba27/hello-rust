9106cf94cebf8c7ccbe7162539e3800790e73052
Grubba27 <70247653+Grubba27@users.noreply.github.com>
2022-01-06 21:41:19 -0300

    Finished tax test

--- src/tax/mod.rs
+++ src/tax/mod.rs
@@ -1,11 +1,11 @@
 pub mod tax {
     ///
     /// Returns the value that was blocked in the source based on this test 1000 > $ -> $ *.10
-    pub fn blocked_in_source_tax(val: f64) -> i32 {
-        if val < 1000 as f64 {
-            0
+    pub fn blocked_in_source_tax(val: f64) -> f64 {
+        (if val < 1000 as f64 {
+            0.0
         } else {
             (val * 0.10)
-        }
+        } as f64)
     }
 }
--- src/tests/tax_test.rs
+++ src/tests/tax_test.rs
@@ -4,4 +4,27 @@ use crate::tax::tax::blocked_in_source_tax;
 mod tax_text {
     use super::*;
 
+    #[test]
+    pub fn money_below_1000_test_max() {
+        let result = blocked_in_source_tax(999.0);
+        assert_eq!(result, 0.0, "Given an input lower than 1000 it should return 0");
+    }
+
+    #[test]
+    pub fn money_below_1000_test_min() {
+        let result = blocked_in_source_tax(1.1);
+        assert_eq!(result, 0.0, "Given an input lower than 1000 it should return 0");
+    }
+
+    #[test]
+    pub fn tax_above_exact_test_max() {
+        let result = blocked_in_source_tax(1000.0);
+        assert_eq!(result, 100.0, "Given an input bigger than 1000 it should return 10%");
+    }
+
+    #[test]
+    pub fn tax_above_exact_test_min() {
+        let result = blocked_in_source_tax(2100.0);
+        assert_eq!(result, 210.0, "Given an input bigger than 1000 it should return 10%");
+    }
 }
