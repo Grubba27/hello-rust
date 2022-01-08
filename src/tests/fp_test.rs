fe9dd7caf438743fb80972c236aa412a91bb5fd0
Grubba27 <70247653+Grubba27@users.noreply.github.com>
2022-01-08 17:10:01 -0300

    Added simple curry

--- src/tests/fp_test.rs
+++ src/tests/fp_test.rs
@@ -1,4 +1,4 @@
-use crate::fp::{calculate_big_salary, calculate_medium_salary, calculate_small_salary};
+use crate::fp::{calculate_big_salary, calculate_medium_salary, calculate_small_salary, curry_function};
 
 #[cfg(test)]
 mod fp_test {
@@ -21,6 +21,11 @@ mod fp_test {
         let result = calculate_big_salary();
         assert_eq!(result, 12.2, "Testing taxes, with small should return .62");
     }
-
+    #[test]
+    pub fn curring_test() {
+        let sum_fn = curry_function(10);
+        let result = sum_fn(90);
+        assert_eq!(result, 100 , "Testing taxes, with small should return .62");
+    }
 
 }
